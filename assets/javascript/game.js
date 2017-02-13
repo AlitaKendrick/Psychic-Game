d34d5546142d4af6bf2c5c1715542cbfcdead54f
Alita Kendrick <alitakendrick@Alitas-MacBook-Pro.local>
2017-02-12 19:22:07 -0600

    working hard/hardlyworking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,53 @@
-
-function compLetter () {
-var random = Math.floor(Math.random() * (letters.length));
-var  randomChoice= letters[random];
-console.log(randomChoice)
-return randomChoice;
+//computer's random choice for letter
+function computerChoice () {
+var random = Math.floor(Math.random() * (letterList.length));
+var  randomLetter= letterList[random];
+console.log(randomLetter)
+return randomLetter;
 }
 
-function guessedLetter () {
-	var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
-	document.getElementById('yourGuess').innerHTML = userChoice 
-}
+// variables 
+var letterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-function resetGuess(){
-	var guesses = 9
-}
-
-//scoreboard variables and links to HTML
 var wins = 0;
 var losses = 0;
-var guesses = 9;
-var message = [];
+var remaining = 9;
 var yourGuess = [];
+var message = [];
+
+
+var computerLetter = computerChoice();
+
 document.getElementById('wins').innerHTML = wins;
 document.getElementById('losses').innerHTML = losses;
-document.getElementById('guesses').innerHTML = guesses;
+document.getElementById('guesses').innerHTML = remaining;
 document.getElementById('yourGuess').innerHTML = yourGuess;
+document.getElementById('message').innerHTML = message;
 
 
-//variables for computer choice
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var computerChoice = compLetter();
-
-
-
-// userchoice and scoring 
+//main function for game
 document.onkeypress = function(event) {
-	var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+  var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
 
+    if (userChoice == computerLetter){
+      Wins++;
+      document.getElementById('wins').innerHTML = wins;
+      document.getElementById("currentGuess").innerHTML = 'Nice! You must be psychic!' + userChoice + ' is the correct letter!';
+      computerLetter = computerChoice();
+      document.getElementById('guesses').innerHTML = (remaining = 9);
+    	}
+    else {
+    	remaining--;
+    	document.getElementById('guesses').innerHTML = remaining;
+    	document.getElementById("yourGuess").innerHTML = userChoice;
+		document.getElementById("message").innerHTML = 'Opps! You have ' + remaining + ' guesses left!';
 
-		if (userChoice == computerChoice){
-			wins++;
-			document.getElementById('wins').innerHTML = wins;
-			document.getElementById('message').innerHTML = 'Great Job! ' + userChoice + ' is right!';
-
-			compLetter();
-
-		}
-		else {
-			guesses-1;
-			document.getElementById('guesses').innerHTML = guesses;
-			guessedLetter ();
-			resetGuess ();
-
-			if (guesses = 0) {
-				losses++;
-				document.getElementById('losses').innerHTML = losses;
-				document.getElementById('message').innerHTML = "Better luck next time! A new letter has been generated";
-				
-				compLetter();
-				resetGuess ();
-			}
-		}
-} 
+     	if (remaining < 1) {
+        	losses++;
+        	document.getElementById('losses').innerHTML = losses;
+        	document.getElementById('currentGuess').innerHTML = "You have run out of guesses!<br /> A new letter has been generated. Try again!"
+        	computerLetter = computerChoice();
+        	document.getElementById('guesses').innerHTML = (remaining = 9);
+      		}
+   		}
+	} 
